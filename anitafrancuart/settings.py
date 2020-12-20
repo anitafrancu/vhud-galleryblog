d8bb5c084d5a9f91c0425e5703edafc5fed0b37b
anitafrancu <70772773+anitafrancu@users.noreply.github.com>
2020-12-20 20:58:38 +0200

    Update settings.py

--- anitafrancuart/settings.py
+++ anitafrancuart/settings.py
@@ -1,7 +1,7 @@
 import os
 import environ
 
-#import django_heroku
+import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 location = lambda x: os.path.join(os.path.dirname(os.path.realpath(__file__)), x)
