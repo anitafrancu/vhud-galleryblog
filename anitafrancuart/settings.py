53790acd671e9aa7b42ba0da411116e495eb079f
anitafrancu <70772773+anitafrancu@users.noreply.github.com>
2021-03-28 21:22:22 +0300

    Update settings.py

--- anitafrancuart/settings.py
+++ anitafrancuart/settings.py
@@ -95,18 +95,6 @@ WSGI_APPLICATION = 'anitafrancuart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '', #'',
-        'USER': '', #'',
-        'PASSWORD': '', #'',
-        'HOST': '', #'',
-        'PORT': '', #'',
-        'SSLMODE': 'require',
-    }
-}
-
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
